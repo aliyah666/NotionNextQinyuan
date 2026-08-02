3610713a72cf335100a399230580c286ce59922f
aliyah666 <qry33120@gmail.com>
2026-08-02 15:09:22 +0800

    修复 /hotel redirect 改用标准 Next.js 写法

--- pages/hotel.js
+++ pages/hotel.js
@@ -2,10 +2,13 @@
  * /hotel 路由 → 转发到 hotel-calendar.html
  * 不修改 NotionNext 任何原有代码，仅新增此文件
  */
-export async function getServerSideProps({ res }) {
-  res.writeHead(302, { Location: '/hotel-calendar.html' });
-  res.end();
-  return { props: {} };
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: '/hotel-calendar.html',
+      permanent: false
+    }
+  };
 }
 
 export default function Hotel() {
